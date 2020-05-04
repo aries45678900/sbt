349424a37c23de63a6edb8f8640faf79365a9cc8
aries45678900 <63440290+aries45678900@users.noreply.github.com>
2020-05-04 12:04:25 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -44,10 +44,10 @@ class Config(object):
         'DEFAULT': '8.11.0'
     }
 
-    APP_TYPE    = 'CHROMEOS'
+    APP_TYPE    = 'DESKTOPWIN'
     APP_VER     = APP_VERSION[APP_TYPE] if APP_TYPE in APP_VERSION else APP_VERSION['DEFAULT']
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'Mbah Google'
+    SYSTEM_NAME = 'RNBOTS'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
